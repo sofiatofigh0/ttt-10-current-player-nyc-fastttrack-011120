647bdd988fad4d3885bb4bb439bb2c8da2790af9
sofia tofigh <sst2140@columbia.edu>
2020-03-09 05:11:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,3 +8,12 @@ board.each do |player|
 end
 turn
 end
+
+def current_player(board)
+  
+ if turn_count==0 ||turn_count==2||turn_count==4||turn_count==6
+   "X"
+ elsif turn_count==1 || turn_count==3 ||turn_count==5
+   "O"
+  end
+end
